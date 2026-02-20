5f8117ed8c820549e9c6ecb753a34d2f4a1e6cab
Kipe <qpkp.dev@gmail.com>
2026-02-19 22:53:57 -0300

    Update misc.lua

--- src/gui/tabs/misc.lua
+++ src/gui/tabs/misc.lua
@@ -1,1 +1,18 @@
+--!strict
 
+export type Tokens = any
+
+local MiscTab = {}
+
+function MiscTab.create(_: Tokens): Frame
+	local frame = Instance.new("Frame")
+	frame.Name = "Tab_MISC"
+	frame.Size = UDim2.new(1, 0, 1, 0)
+	frame.Position = UDim2.new(0, 0, 0, 0)
+	frame.BackgroundTransparency = 1
+	frame.BorderSizePixel = 0
+	frame.Active = false
+	return frame
+end
+
+return MiscTab
