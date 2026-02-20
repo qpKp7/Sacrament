a7c0d4e8b03086adcd47b39244a15d1e3ad65c94
Kipe <qpkp.dev@gmail.com>
2026-02-19 22:56:25 -0300

    Update tab_ids.lua

--- src/navigation/tab_ids.lua
+++ src/navigation/tab_ids.lua
@@ -1,1 +1,11 @@
+--!strict
 
+local UIState = require(script.Parent.Parent.state.ui_state)
+
+export type TabId = UIState.TabId
+
+local TabIds = {}
+
+TabIds.Order = { "COMBAT", "PLAYER", "VISUAL", "MISC", "INFO" } :: { TabId }
+
+return TabIds
