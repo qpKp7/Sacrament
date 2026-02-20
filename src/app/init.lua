852914025bb049acb533d10d318fc8ef72fb7cf9
Kipe <qpkp.dev@gmail.com>
2026-02-20 02:47:43 -0300

    Update init.lua

--- src/app/init.lua
+++ src/app/init.lua
@@ -1,12 +1,18 @@
 --!strict
-local InputHandler = require(script.Parent.input.InputHandler)
-local UIManager = require(script.Parent.gui.UIManager)
+local InputHandler = require(script.Parent.input.inputhandler)
+local UIManager = require(script.Parent.gui.uimanager)
+
+export type Adapter = {
+    mountGui: (gui: ScreenGui) -> (),
+    connectInputBegan: (callback: (InputObject, boolean) -> ()) -> RBXScriptConnection,
+    getViewportSize: () -> Vector2
+}
 
 local App = {}
 
-function App.Start()
-    InputHandler.Init()
-    UIManager.Init()
+function App.Start(adapter: Adapter?)
+    InputHandler.Init(adapter)
+    UIManager.Init(adapter)
 end
 
 function App.Stop()
