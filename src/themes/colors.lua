55447e2e8cbdcc3044fd0ebcc3fc785d01fcc319
Kipe <qpkp.dev@gmail.com>
2026-02-20 02:32:07 -0300

    Create colors.lua

---
+++ src/themes/colors.lua
@@ -0,0 +1,12 @@
+--!strict
+local Colors = {
+    SidebarBackground = Color3.fromRGB(6, 6, 6),
+    ContentBackground = Color3.fromRGB(14, 14, 14),
+    Divider = Color3.fromRGB(150, 0, 0),
+    ButtonDefault = Color3.fromRGB(12, 12, 12),
+    ButtonHover = Color3.fromRGB(20, 20, 20),
+    ButtonActive = Color3.fromRGB(18, 18, 18),
+    TextDefault = Color3.fromRGB(180, 180, 180)
+}
+
+return Colors
