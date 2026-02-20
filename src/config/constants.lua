a63316062088270032abeebd8525bc40eac0c122
Kipe <qpkp.dev@gmail.com>
2026-02-20 02:33:20 -0300

    Update constants.lua

--- src/config/constants.lua
+++ src/config/constants.lua
@@ -1,1 +1,10 @@
+--!strict
+local Constants = {
+    GROUP_ID = 869981416,
+    TOGGLE_KEY = Enum.KeyCode.RightAlt,
+    MODULE_NAMES = {"COMBAT", "PLAYER", "VISUAL", "MISC", "INFO"},
+    SIDEBAR_WIDTH = 0.24,
+    UI_TWEEN_INFO = TweenInfo.new(0.15, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
+}
 
+return Constants
