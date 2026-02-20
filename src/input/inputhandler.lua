afd415f2b7ca5ba9d0e70f4b05179207cd6037d7
Kipe <qpkp.dev@gmail.com>
2026-02-20 02:49:43 -0300

    Update inputhandler.lua

--- src/input/inputhandler.lua
+++ src/input/inputhandler.lua
@@ -1,18 +1,26 @@
 --!strict
 local UserInputService = game:GetService("UserInputService")
-local UIState = require(script.Parent.Parent.state.UIState)
-local Constants = require(script.Parent.Parent.config.Constants)
-local Maid = require(script.Parent.Parent.utils.Maid)
+local UIState = require(script.Parent.Parent.state.uistate)
+local Constants = require(script.Parent.Parent.config.constants)
+local Maid = require(script.Parent.Parent.utils.maid)
 
 local InputHandler = {}
 local handlerMaid = Maid.new()
 
-function InputHandler.Init()
-    handlerMaid:GiveTask(UserInputService.InputBegan:Connect(function(input: InputObject, gameProcessed: boolean)
-        if not gameProcessed and input.KeyCode == Constants.TOGGLE_KEY then
-            UIState.ToggleVisibility()
-        end
-    end))
+function InputHandler.Init(adapter: any?)
+    if adapter and adapter.connectInputBegan then
+        handlerMaid:GiveTask(adapter.connectInputBegan(function(input: InputObject, gameProcessed: boolean)
+            if not gameProcessed and input.KeyCode == Constants.TOGGLE_KEY then
+                UIState.ToggleVisibility()
+            end
+        end))
+    else
+        handlerMaid:GiveTask(UserInputService.InputBegan:Connect(function(input: InputObject, gameProcessed: boolean)
+            if not gameProcessed and input.KeyCode == Constants.TOGGLE_KEY then
+                UIState.ToggleVisibility()
+            end
+        end))
+    end
 end
 
 function InputHandler.Destroy()
