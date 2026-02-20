6c66525aefb299d21fd8ec7181979a25c97e3fda
Kipe <qpkp.dev@gmail.com>
2026-02-19 22:42:39 -0300

    Update instance.lua

--- src/utils/instance.lua
+++ src/utils/instance.lua
@@ -1,1 +1,26 @@
+--!strict
 
+local InstanceUtil = {}
+
+export type Props = { [string]: any }
+export type Children = { Instance }
+
+function InstanceUtil.create(className: string, props: Props?, children: Children?): Instance
+	local inst = Instance.new(className)
+
+	if props then
+		for k, v in pairs(props) do
+			(inst :: any)[k] = v
+		end
+	end
+
+	if children then
+		for _, child in ipairs(children) do
+			child.Parent = inst
+		end
+	end
+
+	return inst
+end
+
+return InstanceUtil
