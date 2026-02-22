94f65ce46b333aba6c61d9694ed5789ea880cf02
Kipe <qpkp.dev@gmail.com>
2026-02-22 00:40:52 -0300

    Update hitchance.lua

--- src/gui/modules/combat/sections/silentaim/hitchance.lua
+++ src/gui/modules/combat/sections/silentaim/hitchance.lua
@@ -1,1 +1,30 @@
+--!strict
+local Import = (_G :: any).SacramentImport
+local Maid = Import("utils/maid")
+local Slider = Import("gui/modules/combat/components/slider")
 
+export type HitChanceSection = {
+    Instance: Frame,
+    Destroy: (self: HitChanceSection) -> ()
+}
+
+local HitChanceFactory = {}
+
+function HitChanceFactory.new(layoutOrder: number): HitChanceSection
+    local maid = Maid.new()
+    
+    local slider = Slider.new("Hit Chance", 0, 100, 95, 1)
+    slider.Instance.LayoutOrder = layoutOrder
+    maid:GiveTask(slider)
+
+    local self = {}
+    self.Instance = slider.Instance
+
+    function self:Destroy()
+        maid:Destroy()
+    end
+
+    return self
+end
+
+return HitChanceFactory
