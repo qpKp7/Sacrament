dadabd067822237a20d539448594d30fbf72d34f
Kipe <qpkp.dev@gmail.com>
2026-02-22 21:55:34 -0300

    Update silentaim.lua

--- src/gui/modules/combat/silentaim.lua
+++ src/gui/modules/combat/silentaim.lua
@@ -122,8 +122,8 @@ function SilentAimFactory.new(): SilentAimUI
         local titleRightAbsolute = title.AbsolutePosition.X + title.AbsoluteSize.X
         local controlsLeftAbsolute = controls.AbsolutePosition.X
 
-        local startX = (titleRightAbsolute - header.AbsolutePosition.X) + 3
-        local endX = (controlsLeftAbsolute - header.AbsolutePosition.X) - 3
+        local startX = (titleRightAbsolute - header.AbsolutePosition.X) + 5
+        local endX = (controlsLeftAbsolute - header.AbsolutePosition.X) - 5
 
         local width = math.max(0, endX - startX)
 
