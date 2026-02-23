f2b4504e0fd0fabb2befab3883ba5cc86807e91f
Kipe <qpkp.dev@gmail.com>
2026-02-22 22:43:56 -0300

    Update aimlock.lua

--- src/gui/modules/combat/aimlock.lua
+++ src/gui/modules/combat/aimlock.lua
@@ -51,7 +51,8 @@ function AimlockFactory.new(): AimlockUI
 
     local title = Instance.new("TextLabel")
     title.Name = "Title"
-    title.Size = UDim2.fromOffset(130, 50)
+    title.Size = UDim2.fromOffset(0, 50)
+    title.AutomaticSize = Enum.AutomaticSize.X
     title.Position = UDim2.fromOffset(20, 0)
     title.BackgroundTransparency = 1
     title.Text = "Aimlock"
