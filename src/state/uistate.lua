dc00775e12a5bf04039adaf2739ac00b7d38cf6f
Kipe <qpkp.dev@gmail.com>
2026-03-08 23:45:05 -0300

    Update uistate.lua

--- src/state/uistate.lua
+++ src/state/uistate.lua
@@ -5,10 +5,13 @@ local Constants = Import("config/constants")
 
 local UIState = {
     IsVisible = true,
-    ActiveTab = Constants.MODULE_NAMES[1],
+    ActiveTab = Constants.MODULE_NAMES and Constants.MODULE_NAMES[1] or "Combat",
+    
+    Settings = {} :: { [string]: any },
     
     VisibilityChanged = Signal.new() :: Signal.Signal<boolean>,
-    TabChanged = Signal.new() :: Signal.Signal<string>
+    TabChanged = Signal.new() :: Signal.Signal<string>,
+    SettingChanged = Signal.new() :: Signal.Signal<string, any>
 }
 
 function UIState.ToggleVisibility()
@@ -21,6 +24,20 @@ function UIState.SetActiveTab(tabName: string)
         UIState.ActiveTab = tabName
         UIState.TabChanged:Fire(tabName)
     end
+end
+
+function UIState.Set(key: string, value: any)
+    if UIState.Settings[key] ~= value then
+        UIState.Settings[key] = value
+        UIState.SettingChanged:Fire(key, value)
+    end
+end
+
+function UIState.Get(key: string, defaultValue: any?): any
+    if UIState.Settings[key] == nil and defaultValue ~= nil then
+        UIState.Settings[key] = defaultValue
+    end
+    return UIState.Settings[key]
 end
 
 return UIState
