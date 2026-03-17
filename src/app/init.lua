23ddcaf3bb107add92a880b9995f85df6fbc56b1
Kipe <qpkp.dev@gmail.com>
2026-03-17 13:03:48 -0300

    Update init.lua

--- src/app/init.lua
+++ src/app/init.lua
@@ -1,19 +1,24 @@
-local Import = _G.SacramentImport
+--!strict
+local Import = (_G :: any).SacramentImport
 
 local InputHandler = Import("input/inputhandler")
 local UIManager = Import("gui/uimanager")
 local Settings = Import("logic/settings")
 
+export type Adapter = {
+    mountGui: (gui: ScreenGui) -> (),
+    connectInputBegan: (callback: (InputObject, boolean) -> ()) -> RBXScriptConnection,
+    getViewportSize: () -> Vector2
+}
+
 local App = {}
 
-function App.Start(adapter)
-    -- Inicia os módulos repassando as funções do executor
+function App.Start(adapter: Adapter)
     InputHandler.Init(adapter)
     UIManager.Init(adapter, Settings)
 end
 
 function App.Stop()
-    -- Garante que tudo seja destruído ao desligar o script
     InputHandler.Destroy()
     UIManager.Destroy()
 end
